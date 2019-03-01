0365ef53c961655cd56b0bbf74715f4f50c8f767
子勰 <code@bihe0832.com>
2019-03-01 10:23:43 +0800

    https

--- crossDomain/server/crossdomain.php
+++ crossDomain/server/crossdomain.php
@@ -2,6 +2,7 @@
 $data= Array();
 $data["ecode"]="200";
 $data["msg"]="努力学技术，潜心做精品";
+header("Access-Control-Allow-Origin: https://microdemo.bihe0832.com");
 header("Access-Control-Allow-Origin: http://microdemo.bihe0832.com");
 $result = json_encode($data);
 echo $result; 
